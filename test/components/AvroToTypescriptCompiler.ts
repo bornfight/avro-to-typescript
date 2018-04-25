3d2445fd74fcaa62a71df33adcfbdc143f283c12
Andrija Dvorski <dvorsky.andrija@gmail.com>
2018-04-25 10:57:14 +0200

    I fix tests, not code

--- test/components/AvroToTypescriptCompiler.ts
+++ test/components/AvroToTypescriptCompiler.ts
@@ -1,5 +1,5 @@
 import * as chai from "chai";
-import * as fs from "fs";
+import * as fs from "fs-extra";
 import * as path from "path";
 import sinonChai = require("sinon-chai");
 import {AvroToTypescriptCompiler} from "../../src/components/AvroToTypescriptCompiler";
@@ -15,7 +15,7 @@ const avscRoot = `${root}/test/data/avscFieldsData`;
 
 describe("Testing AvroToTypescriptCompiler", () => {
 
-    it.skip(`should create file ./data/tsCompiled/testAvscSchemaUser.ts with same content as in
+    it(`should create file ./data/tsCompiled/testAvscSchemaUser.ts with same content as in
     ./data/expectedTsTypes/testAvscSchemaUser.ts
     when given testAvscSchemaUser.json`, async () => {
 
@@ -36,12 +36,18 @@ describe("Testing AvroToTypescriptCompiler", () => {
         const mainCompiled = new AvroToTypescriptCompiler();
         mainCompiled.tsSchemaPath = tsCompiledPath;
         mainCompiled.avroSchemaPath = avscPath;
-        await mainCompiled.compileFolder();
+
+        const data = fs.readFileSync(avscPath).toString()
+
+        const info = await mainCompiled.compile(JSON.parse(data));
 
         const tsCompiledContent: string = fs.readFileSync(tsCompiledPath).toString();
         const tsExpectedContent: string = fs.readFileSync(tsExpectedPath).toString();
 
         expect(tsCompiledContent).equal(tsExpectedContent);
+
+        fs.removeSync(info.dir + "/../");
+
         return;
     }
 
