c0e8adebc66a4512c830e5e8299d867c4986c15a
Andrija Dvorski <dvorsky.andrija@gmail.com>
2018-06-08 16:19:59 +0200

    refactor(tests): Rework RecordConverter Tests (#7)
    Refactor of these tests goes along with refactor of whole system, so
    they wont work on their own. Aim was to have cleaner tests that can be
    used as example.

---
+++ test/components/RecordConverterTest.ts
@@ -0,0 +1,75 @@
+import * as chai from "chai";
+import * as fs from "fs";
+import * as path from "path";
+import sinonChai = require("sinon-chai");
+import { RecordConverter } from "../../src";
+
+const expect = chai.expect;
+
+chai.should();
+chai.use(sinonChai);
+
+const dataFolder = path.resolve(`./test/data/`);
+const avroFolder = path.resolve(dataFolder + `/avro/`);
+const compiledFolder = path.resolve(dataFolder + `/compiled/`);
+
+const getExpectedResult = (file: string) => {
+    return fs.readFileSync(file).toString();
+};
+
+describe("Record Converter test", () => {
+    it("should convert simple avro schema to TS interface", () => {
+        const converter = new RecordConverter();
+        converter.convert(`${avroFolder}/SimpleRecord.avsc`);
+
+        const actual = converter.joinExports();
+        const expected = getExpectedResult(`${compiledFolder}/SimpleRecord.ts.test`);
+        expect(actual).to.deep.equal(expected);
+    });
+
+    it("should convert simple avro schema json to TS interface", () => {
+        const converter = new RecordConverter();
+        const avro = JSON.parse(fs.readFileSync(`${avroFolder}/SimpleRecord.json`).toString());
+        converter.convert(avro);
+
+        const actual = converter.joinExports();
+        const expected = getExpectedResult(`${compiledFolder}/SimpleRecord.ts.test`);
+        expect(actual).to.deep.equal(expected);
+    });
+
+    it("should convert avro schema with interface to TS interface", () => {
+        const converter = new RecordConverter();
+        converter.convert(`${avroFolder}/RecordWithinterface.avsc`);
+
+        const actual = converter.joinExports();
+        const expected = getExpectedResult(`${compiledFolder}/RecordWithinterface.ts.test`);
+        expect(actual).to.deep.equal(expected);
+    });
+
+    it("should convert avro schema with Map type to TS interface", () => {
+        const converter = new RecordConverter();
+        converter.convert(`${avroFolder}/RecordWithMap.avsc`);
+
+        const actual = converter.joinExports();
+        const expected = getExpectedResult(`${compiledFolder}/RecordWithMap.ts.test`);
+        expect(actual).to.deep.equal(expected);
+    });
+
+    it("should convert avro schema with all types to TS interface", () => {
+        const converter = new RecordConverter();
+        converter.convert(`${avroFolder}/RecordWithUnion.avsc`);
+
+        const actual = converter.joinExports();
+        const expected = getExpectedResult(`${compiledFolder}/RecordWithUnion.ts.test`);
+        expect(actual).to.deep.equal(expected);
+    });
+
+    it("should convert avro schema with Map type to TS interface", () => {
+        const converter = new RecordConverter();
+        converter.convert(`${avroFolder}/ComplexRecord.avsc`);
+
+        const actual = converter.joinExports();
+        const expected = getExpectedResult(`${compiledFolder}/ComplexRecord.ts.test`);
+        expect(actual).to.deep.equal(expected);
+    });
+});
