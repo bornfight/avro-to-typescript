b69b2a326e56e876c52a33eed5cf2205714ce5ad
Andrija Dvorski <dvorsky.andrija@gmail.com>
2018-06-08 17:56:54 +0200

    refactor(tests): Rework PrimitiveConverter Tests (#7)
    Refactor of these tests goes along with refactor of whole system, so
    they wont work on their own. Aim was to have cleaner tests that can be
    used as example.

---
+++ test/components/PrimitiveConverter.ts
@@ -0,0 +1,47 @@
+import * as chai from "chai";
+import { PrimitiveConverter } from "../../src";
+
+const expect = chai.expect;
+
+chai.should();
+
+describe("Compile primitive types", () => {
+
+    const primitivesMap: any = {
+        number: [
+            "int",
+            "long",
+            "double",
+            "float",
+        ],
+        Buffer: [
+            "bytes",
+        ],
+        null: [
+            "null",
+        ],
+        boolean: [
+            "boolean",
+        ],
+        string: [
+            "string",
+        ],
+    };
+
+    for (const expected in primitivesMap) {
+        if (primitivesMap.hasOwnProperty(expected) === false) {
+            continue;
+        }
+
+        for (const primitive of primitivesMap[expected]) {
+
+            it(`should return ${expected} when given ${primitive}`, () => {
+                const converter = new PrimitiveConverter();
+                const actual = converter.convert(primitive);
+
+                expect(expected).equal(actual);
+            });
+
+        }
+    }
+});
