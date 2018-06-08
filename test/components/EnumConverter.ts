4687cb226e9b763171cf59070939037ee5ea6560
Andrija Dvorski <dvorsky.andrija@gmail.com>
2018-06-08 17:44:41 +0200

    refactor(tests): Rework EnumConverter Tests (#7)
    Refactor of these tests goes along with refactor of whole system, so
    they wont work on their own. Aim was to have cleaner tests that can be
    used as example.

---
+++ test/components/EnumConverter.ts
@@ -0,0 +1,31 @@
+import * as chai from "chai";
+import * as fs from "fs";
+import * as path from "path";
+import { EnumConverter } from "../../src";
+
+import sinonChai = require("sinon-chai");
+
+const expect = chai.expect;
+
+chai.should();
+chai.use(sinonChai);
+
+const dataFolder = path.resolve(`./test/data/`);
+const avroFolder = path.resolve(dataFolder + `/avro/`);
+const compiledFolder = path.resolve(dataFolder + `/compiled/`);
+
+const getExpectedResult = (file: string) => {
+    return fs.readFileSync(file).toString();
+};
+
+describe("Enum Converter", () => {
+
+    it(`should successfully convert Enum avro file to TS enum`, () => {
+        const converter = new EnumConverter();
+        converter.convert(`${avroFolder}/SimpleEnum.avsc`);
+
+        const actual = converter.joinExports();
+        const expected = getExpectedResult(`${compiledFolder}/SimpleEnum.ts.test`);
+        expect(actual).to.deep.equal(expected);
+    });
+});
---
+++ test/data/compiled/SimpleEnum.ts.test
@@ -0,0 +1,5 @@
+export enum OAuthStatus {
+    test1,
+    test2,
+    test3,
+}
