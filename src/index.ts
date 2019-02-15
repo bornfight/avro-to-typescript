382dff5b02f9154c68ca697b2c79fef9b7691346
Andrija Dvorski <dvorsky.andrija@gmail.com>
2019-02-15 14:08:20 +0100

    bugfix(BaseAvroRecord): Export BaseAvroRecord from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./components/Avro/AvroRecord";
 export * from "./components/Avro/BaseAvroRecord";
 export * from "./components/Avro/KafkaAvroMessage";
+export * from "./components/Avro/serde/BaseKafkaAvroSerde";
 export * from "./components/Avro/serde/KafkaAvroDeserializer";
 export * from "./components/Avro/serde/KafkaAvroFactory";
 export * from "./components/Avro/serde/KafkaAvroSerializer";
@@ -8,6 +9,7 @@ export * from "./components/Avro/serde/Serializable";
 export * from "./components/Compiler/Compiler";
 export * from "./components/Converters/ClassConverter";
 export * from "./components/Converters/EnumConverter";
+export * from "./components/Converters/LogicalTypeConverter";
 export * from "./components/Converters/PrimitiveConverter";
 export * from "./components/Converters/RecordConverter";
 export * from "./interfaces/AvroSchema";
